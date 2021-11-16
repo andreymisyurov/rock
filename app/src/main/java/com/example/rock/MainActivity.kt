fb1195df7197557a951a8544137a3c1249babaab
gonzo <andrey.misyrov@gmail.com>
2021-11-16 18:02:34 +0300

    “firstblood”

--- app/src/main/java/com/example/rock/MainActivity.kt
+++ app/src/main/java/com/example/rock/MainActivity.kt
@@ -10,6 +10,7 @@ import android.widget.TextView
 import kotlin.random.Random
 import kotlin.random.nextInt
 import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 
 
 private const val BITCH_TAG = "BitchTag"
@@ -21,34 +22,39 @@ class MainActivity : AppCompatActivity() {
     private lateinit var playbutton: Button
     private lateinit var manualbutton: Button
     private lateinit var youtubebutton: Button
+    private lateinit var rock: TextView
+    private lateinit var scissor: TextView
+    private lateinit var paper: TextView
+    lateinit var nextbutton: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        playbutton = findViewById(R.id.button_centr)
+        playbutton = findViewById(R.id.playbutton_centr)
         playtext = findViewById(R.id.text_centr)
         playtext.textSize = (55F)
+     //   scissor.text = "2"
 
         playbutton.setOnClickListener {
             randomize()
+
         }
 
         if (savedInstanceState != null) {
             playtext.text = savedInstanceState.getString(VALUES)
         }
 
-        manualbutton = findViewById(R.id.button_topleft)
 
+        manualbutton = findViewById(R.id.button_topleft)
         val manualintent = Intent(
             Intent.ACTION_VIEW,
             Uri.parse("https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D0%BC%D0%B5%D0%BD%D1%8C,_%D0%BD%D0%BE%D0%B6%D0%BD%D0%B8%D1%86%D1%8B,_%D0%B1%D1%83%D0%BC%D0%B0%D0%B3%D0%B0")
         )
-
-
         manualbutton.setOnClickListener {
             startActivity(manualintent)
+
         }
 
         youtubebutton = findViewById(R.id.button_topright)
@@ -57,8 +63,18 @@ class MainActivity : AppCompatActivity() {
             Intent.ACTION_VIEW,
             Uri.parse("http://www.youtube.com/watch?v=" + "-6fS2ndNs1U")
         )
+        //youtubeintent.flags = FLAG_ACTIVITY_NEW_TASK
         youtubebutton.setOnClickListener {
             startActivity(youtubeintent)
+        }
+
+        nextbutton = findViewById(R.id.nextslide)
+
+        val intentbutton = Intent(this, manualgame::class.java)
+
+        nextbutton.setOnClickListener{
+
+            startActivity(intentbutton)
         }
 
     }
--- app/src/main/java/com/example/rock/manualgame.kt
+++ app/src/main/java/com/example/rock/manualgame.kt
@@ -1,11 +1,23 @@
 package com.example.rock
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class manualgame : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manualgame)
+
+        val backbutton: Button = findViewById(R.id.backbutton)
+
+        val intentback = Intent(this, MainActivity::class.java)
+
+        backbutton.setOnClickListener{
+            startActivity(intentback)
+
+
+        }
     }
 }
